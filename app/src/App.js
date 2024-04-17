2b6de899a7448f4632abeb72a657c37f133ee657
Chipollinos <psudnikovic@gmail.com>
2024-04-17 17:20:13 +0300

    .

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,17 @@
 import HomePage from "./pages/home/home"
+import { BrowserRouter,  Routes, Route} from "react-router-dom"
+import Header from "./component/header/header"
 
 function App() {
-    return <HomePage/>
+    return <BrowserRouter>
+    <Routes>
+        <Route path="/home" Component={HomePage}/>
+    </Routes>
+    <Routes>
+        <Route path="/search" Component={Header}/>
+    </Routes>
+    </BrowserRouter> 
+    // <HomePage/>
 }
 
 export default App
