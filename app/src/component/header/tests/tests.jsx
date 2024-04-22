0aae5dc4341c83936907436d078a80a5769b52e8
Chipollinos <psudnikovic@gmail.com>
2024-04-22 21:37:44 +0300

    .

--- app/src/component/header/tests/tests.jsx
+++ app/src/component/header/tests/tests.jsx
@@ -2,7 +2,14 @@ import style from './tests.module.css'
 
 const Tests = () => {
     return <section className={style.section}>
-        <h1>po</h1>
+    <div className={style.categories}>
+      <h2>Категории тестов:</h2>
+      <ul>
+        <li>Категория 1</li>
+        <li>Категория 2</li>
+        <li>Категория 3</li>
+      </ul>
+    </div>
     </section>
 }
 
